68fcc205fb9d83df718b3f348852b4351baddf86
skka <2569877795@qq.com>
2019-12-18 23:23:46 +0800

    init

---
+++ src/main/java/com/partern/ParternApplication.java
@@ -0,0 +1,13 @@
+package com.partern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/partern/ParternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
