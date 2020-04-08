5d3353dd0524bc9859967b52ccfc8191da827d5c
18839779221 <53285846+18839779221@users.noreply.github.com>
2020-04-08 16:55:58 +0800

    Update HelloController.java

--- src/main/java/com/partern/controller/HelloController.java
+++ src/main/java/com/partern/controller/HelloController.java
@@ -4,6 +4,7 @@ import com.partern.bean.User;
 import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import com.partern.responsebo.responseenitiy.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.ContextLoader;
