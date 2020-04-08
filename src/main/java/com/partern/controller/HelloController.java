3a7d0bfe3e6e891f4fcfbb0cf2729a2438a8033c
18839779221 <53285846+18839779221@users.noreply.github.com>
2020-04-08 16:38:48 +0800

    Update HelloController.java

--- src/main/java/com/partern/controller/HelloController.java
+++ src/main/java/com/partern/controller/HelloController.java
@@ -12,7 +12,7 @@ import org.springframework.web.context.ContextLoader;
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello(){
-        return "main";
+    public ResponseEntity hello(){
+        return ResponseEntity.getPublicSuccessResponse(null);
     }
 }
